50e85a2093d31098f935eff9bc3c020e015c919b
razboynikov <razboynikov@skbkontur.ru>
2018-07-09 10:51:24 +0500

    Code cleaning. Added JetBrains attributes.

--- Vostok.Logging.Context/ContextualLogPrefix.cs
+++ Vostok.Logging.Context/ContextualLogPrefix.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Collections.Immutable;
+using JetBrains.Annotations;
 using Vostok.Context;
 
 namespace Vostok.Logging.Context
 {
-    /// <summary>
-    /// This only works well on .NET 4.5 and later. Avoid using contextual prefixes with older runtime versions.
-    /// </summary>
     public class ContextualLogPrefix : IDisposable
     {
         public const string PrefixKey = "logging.prefix";
         private readonly bool needRestoreContext;
         private readonly ImmutableArray<string> oldPrefix;
 
-        public ContextualLogPrefix(string prefix, bool needRestoreContext = true)
+        public ContextualLogPrefix([NotNull] string prefix, bool needRestoreContext = true)
         {
             FlowingContext.SetOverwriteMode(true);
             this.needRestoreContext = needRestoreContext;
