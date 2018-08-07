eb20960ea82015c16578a0c775b90ec24e164a8a
iloktionov <iloktionov@skbkontur.ru>
2018-08-08 01:07:49 +0500

    First dirty draft of contextual log prefix implementation.

---
+++ Vostok.Logging.Context/ContextualLogPrefix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JetBrains.Annotations;
+using Vostok.Context;
+
+namespace Vostok.Logging.Context
+{
+    // TODO(iloktionov): xml docs
+    // TODO(iloktionov): unit tests
+
+    [PublicAPI]
+    public struct ContextualLogPrefix : IDisposable
+    {
+        private readonly ContextualPrefixValue old;
+
+        public ContextualLogPrefix([NotNull] string value)
+            => FlowingContext.Globals.Set((old = FlowingContext.Globals.Get<ContextualPrefixValue>()) + value);
+
+        [CanBeNull]
+        public static string Current 
+            => FlowingContext.Globals.Get<ContextualPrefixValue>().Value;
+
+        public static void Drop()
+            => FlowingContext.Globals.Set(default(ContextualPrefixValue));
+
+        public void Dispose()
+            => FlowingContext.Globals.Set(old);
+    }
+}
