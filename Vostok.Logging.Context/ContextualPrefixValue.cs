84630d1479c5d6eac16f6c47af726a584a2f5aa1
iloktionov <iloktionov@skbkontur.ru>
2018-08-08 01:07:49 +0500

    First dirty draft of contextual log prefix implementation.

---
+++ Vostok.Logging.Context/ContextualPrefixValue.cs
@@ -0,0 +1,17 @@
+﻿namespace Vostok.Logging.Context
+{
+    // TODO(iloktionov): unit tests
+
+    internal class ContextualPrefixValue
+    {
+        private ContextualPrefixValue(string value)
+            => Value = value;
+
+        public string Value { get; }
+
+        public override string ToString() => Value;
+
+        public static ContextualPrefixValue operator+(ContextualPrefixValue prefix, string append)
+           => new ContextualPrefixValue(string.Concat(prefix?.Value ?? string.Empty, "[", append ?? string.Empty, "] "));
+    }
+}
