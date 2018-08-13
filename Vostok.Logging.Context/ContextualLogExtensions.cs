cfecc2e527139a68c56b27987a9e867200491ce1
iloktionov <iloktionov@skbkontur.ru>
2018-08-13 20:16:35 +0500

    ContextualLogExtensions: implemented all extensions and added xml-docs.

--- Vostok.Logging.Context/ContextualLogExtensions.cs
+++ Vostok.Logging.Context/ContextualLogExtensions.cs
@@ -6,37 +6,89 @@ using Vostok.Logging.Abstractions;
 
 namespace Vostok.Logging.Context
 {
-    // TODO(iloktionov): allow overwrite properties?
-    // TODO(iloktionov): maybe we shouldn't add null properties to log events?
-    // TODO(iloktionov): xml docs
     // TODO(iloktionov): unit tests
 
     [PublicAPI]
     public static class ContextualLogExtensions
     {
-        public static ILog WithFlowingContextGlobal<T>([NotNull] this ILog log, [NotNull] string logPropertyName)
+        /// <summary>
+        /// <para>Returns a wrapper log that adds a value of global property of type <typeparamref name="T"/> from <see cref="FlowingContext"/> to each <see cref="LogEvent"/> before logging.</para>
+        /// <para>Uses given <paramref name="logPropertyName"/> as a name for added <see cref="LogEvent"/> property.</para>
+        /// <para>By default, existing properties are not overwritten. This can be changed via <paramref name="allowOverwrite"/> parameter.</para>
+        /// <para>By default, <c>null</c> values are not added to events. This can be changed via <paramref name="allowNullValues"/> parameter.</para>
+        /// </summary>
+        public static ILog WithFlowingContextGlobal<T>(
+            [NotNull] this ILog log, 
+            [NotNull] string logPropertyName, 
+            bool allowOverwrite = false,
+            bool allowNullValues = false)
         {
-            return log.WithProperty(logPropertyName, () => FlowingContext.Globals.Get<T>());
+            // TODO(iloktionov): implement allowNullValues
+
+            return log.WithProperty(logPropertyName, () => FlowingContext.Globals.Get<T>(), allowOverwrite);
         }
 
-        public static ILog WithFlowingContextProperty([NotNull] this ILog log, [NotNull] string contextPropertyName, [CanBeNull] string logPropertyName = null)
+        /// <summary>
+        /// <para>Returns a wrapper log that adds a value of property with name = <paramref name="contextPropertyName"/> from <see cref="FlowingContext"/> to each <see cref="LogEvent"/> before logging.</para>
+        /// <para>By default, the value is added to <see cref="LogEvent"/> with <paramref name="contextPropertyName"/>. This can be changed by providing a non-null <paramref name="logPropertyName"/> parameter.</para>
+        /// <para>By default, existing properties are not overwritten. This can be changed via <paramref name="allowOverwrite"/> parameter.</para>
+        /// <para>By default, <c>null</c> values are not added to events. This can be changed via <paramref name="allowNullValues"/> parameter.</para>
+        /// </summary>
+        public static ILog WithFlowingContextProperty(
+            [NotNull] this ILog log, 
+            [NotNull] string contextPropertyName, 
+            [CanBeNull] string logPropertyName = null, 
+            bool allowOverwrite = false,
+            bool allowNullValues = false)
         {
-            throw new NotImplementedException();
+            // TODO(iloktionov): implement allowNullValues
+
+            return log.WithProperty(logPropertyName ?? contextPropertyName, () => GetContextPropertyOrNull(contextPropertyName), allowOverwrite);
         }
 
-        public static ILog WithFlowingContextProperties([NotNull] this ILog log, [NotNull] params string[] names)
+        /// <summary>
+        /// <para>Returns a wrapper log that adds values of all properties with names in given <paramref name="names"/> array from <see cref="FlowingContext"/> to each <see cref="LogEvent"/> before logging.</para>
+        /// <para>By default, existing properties are not overwritten. This can be changed via <paramref name="allowOverwrite"/> parameter.</para>
+        /// <para>By default, <c>null</c> values are not added to events. This can be changed via <paramref name="allowNullValues"/> parameter.</para>
+        /// </summary>
+        public static ILog WithFlowingContextProperties(
+            [NotNull] this ILog log, 
+            [NotNull] string[] names, 
+            bool allowOverwrite = false,
+            bool allowNullValues = false)
         {
-            throw new NotImplementedException();
+            // TODO(iloktionov): implement allowNullValues
+
+            return log.WithProperties(() => names.Select(name => (name, GetContextPropertyOrNull(name))), allowOverwrite);
         }
 
-        public static ILog WithAllFlowingContextProperties([NotNull] this ILog log)
+        /// <summary>
+        /// <para>Returns a wrapper log that adds values of all named properties from <see cref="FlowingContext"/> to each <see cref="LogEvent"/> before logging.</para>
+        /// <para>By default, existing properties are not overwritten. This can be changed via <paramref name="allowOverwrite"/> parameter.</para>
+        /// <para>By default, <c>null</c> values are not added to events. This can be changed via <paramref name="allowNullValues"/> parameter.</para>
+        /// </summary>
+        public static ILog WithAllFlowingContextProperties(
+            [NotNull] this ILog log, 
+            bool allowOverwrite = false,
+            bool allowNullValues = false)
         {
-            return log.WithProperties(() => FlowingContext.Properties.Current.Select(pair => (pair.Key, pair.Value)));
+            // TODO(iloktionov): implement allowNullValues
+
+            return log.WithProperties(() => FlowingContext.Properties.Current.Select(pair => (pair.Key, pair.Value)), allowOverwrite);
         }
 
+        /// <summary>
+        /// <para>Returns a wrapper log that adds a <see cref="WellKnownProperties.ContextualPrefix"/> property from <see cref="FlowingContext"/> to each <see cref="LogEvent"/> before logging.</para>
+        /// <para>See <see cref="ContextualLogPrefix"/> for more info.</para>
+        /// </summary>
         public static ILog WithContextualPrefix([NotNull] this ILog log)
         {
             return log.WithProperty(WellKnownProperties.ContextualPrefix, () => ContextualLogPrefix.Current, true);
         }
+
+        private static object GetContextPropertyOrNull(string name)
+        {
+            return FlowingContext.Properties.Current.TryGetValue(name, out var value) ? value : null;
+        } 
     }
 }
