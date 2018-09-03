a74de662c89089f4a416329b0f51b79997bc8a98
Krait <krait@skbkontur.ru>
2018-09-03 18:46:33 +0500

    Revert "Removed extra trailing space."
    This reverts commit de636aeee46ac0ec97a1daf533ef21e46aeb5b72.

--- Vostok.Logging.Context/ContextualPrefixValue.cs
+++ Vostok.Logging.Context/ContextualPrefixValue.cs
@@ -10,6 +10,6 @@
         public override string ToString() => Value;
 
         public static ContextualPrefixValue operator+(ContextualPrefixValue prefix, string append)
-           => new ContextualPrefixValue(string.Concat(prefix?.Value ?? string.Empty, "[", append ?? string.Empty, "]"));
+           => new ContextualPrefixValue(string.Concat(prefix?.Value ?? string.Empty, "[", append ?? string.Empty, "] "));
     }
 }
